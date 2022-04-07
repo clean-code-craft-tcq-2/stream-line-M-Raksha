1c02ea755e5b2c2aa600c69d35905e10c2f83a31
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-04-07 10:57:55 +0530

    Update sender.h

--- sender.h
+++ sender.h
@@ -6,3 +6,8 @@ using namespace std;
 #define MIN_CURRENT 1 
 #define MAX_TEMPERATURE 400
 #define MIN_TEMPERATURE 30
+
+float generateRandomSimulatedData(int max_value, int min_value, unsigned seed);
+string formatdataToCVN(float currentsample, float temperature);
+bool streamOnConsole(string DataTobeStreamed);
+bool GetdataAndStreamOnCOnsole(unsigned noOfSamplesTobeRead);
