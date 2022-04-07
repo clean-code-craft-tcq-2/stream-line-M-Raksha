7629a1d8a8edaf645320c6a6425516307037de93
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-04-07 11:03:17 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -19,4 +19,25 @@ TEST_CASE("Genearte Random data")
     }
 
 }
-     
+ //test data format in CVN   
+TEST_CASE("format data to cvn")
+{
+
+    float cuurentSample = 34;
+    float temperature = 80;
+    REQUIRE(formatdataToCVN(cuurentSample,temperature) == "34.000000,80.000000");
+}
+
+ //test streaming data on console
+TEST_CASE("stream data on console")
+{
+    string DataTobeStreamed = to_string(58.4)+","+to_string(300);
+    REQUIRE(streamOnConsole(DataTobeStreamed)== true);
+}
+
+//get currentValueand temperature from random generator and stream on console
+TEST_CASE("get data and stream on console")
+{
+    unsigned noOfSamplesTobeRead = 50;
+    REQUIRE(GetdataAndStreamOnCOnsole(noOfSamplesTobeRead)== true);
+}
