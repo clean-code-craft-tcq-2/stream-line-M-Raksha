dbed9ebda35cd05690a55482a60ff1e44f8cba7c
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-04-07 10:33:47 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -1,13 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
+#include "sender.h"
+
 
 // test random generator
     float random_Data[30];
-    for(int i = 0;i<30;i++)
+    int seed
+    for(seed=0;seed<30;seed++)
     {
         
-      random_Data[i] = generateRandomSimulatedData(MAX_CURRENT,MIN_CURRENT,i);
+      random_Data[i] = generateRandomSimulatedData(MAX_CURRENT,MIN_CURRENT,seed);
       REQUIRE(random_Data[i] != 0);
       
     }
