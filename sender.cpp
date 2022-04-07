0d2168041140d0727e198e0ac3566754e597fac2
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-04-07 11:04:01 +0530

    Update sender.cpp

--- sender.cpp
+++ sender.cpp
@@ -12,3 +12,35 @@ float generateRandomSimulatedData(int max_value, int min_value, unsigned seed)
   }
    return data;
 }
+
+
+string formatdataToCVN(float currentsample, float temperature)
+{
+    string dataToCVN = to_string(currentsample)+","+to_string(temperature);
+    return dataToCVN;
+}
+
+bool streamOnConsole(string DataTobeStreamed)
+{
+    bool DatastreamingSuccess = true;
+    cout<<DataTobeStreamed<<endl;
+    return DatastreamingSuccess;
+}
+
+bool GetdataAndStreamOnCOnsole(unsigned noOfSamplesTobeRead)
+{
+    unsigned loopCounter;
+    float currentValue;
+    float temperature;
+    string formatttedData;
+    bool retVal;
+    for(loopCounter =0; loopCounter< noOfSamplesTobeRead; loopCounter++)
+    {
+       currentValue = generateRandomSimulatedData(MAX_CURRENT,MIN_CURRENT,loopCounter);
+       temperature = generateRandomSimulatedData(MAX_TEMPERATURE,MIN_TEMPERATURE,loopCounter);
+       formatttedData = formatdataToCVN(currentValue,temperature);
+       retVal=streamOnConsole(formatttedData);
+    }
+    
+    return retVal;
+}
