23c634f4655c1aa5d0b409d15706523e3198f14b
Sudhir kumar <58461255+kumarSudhirTCQ@users.noreply.github.com>
2022-04-24 02:13:24 +0530

    Update reciever.test.py

--- reciever.test.py
+++ reciever.test.py
@@ -3,10 +3,7 @@ import reciever
 import os
 
 class reciever_test(unittest.TestCase):
-    data = "SOC,10 20 50 100 80 90 60 44 15 54 65 54 10 20 50 100 80 90 60 44 15 54 65 54 10 20 50 100 80 90 60 44 15 54 65 54 10 20 50 100 80 90 60 44 15 54 65 54 88 66\nTEMPERATURE,1 4 7 9 2 5 2 20 15 30 1 4 7 9 2 5 2 20 15 30 1 4 7 9 2 5 2 20 15 30 1 4 7 9 2 5 2 20 15 30 1 4 7 9 2 5 2 20 15 30"
-
-    def test_split_data_from_sender(self):
-        consoleData = """58.400000,300
+    consoleData = """58.400000,300
 29.000000,309.000000
 29.000000,309.000000
 43.000000,137.000000
@@ -16,30 +13,30 @@ class reciever_test(unittest.TestCase):
 27.000000,53.000000
 48.000000,350.000000
 59.000000,350.000000"""
-
-        soc_list,temp_list = reciever.split_data_from_sender(consoleData)
+    
+    def test_split_data_from_sender(self):
+        soc_list,temp_list = reciever.split_data_from_sender(self.consoleData)
         self.assertEqual(soc_list, [58, 29, 29, 43, 34, 43, 33, 27, 48, 59])
         self.assertEqual(temp_list, [300, 309, 309, 137, 376, 376, 325, 53, 350, 350])
     
-    '''def test_compute_statitics(self):
-        soc_list,temp_list = reciever.split_data_from_sender(self.data)
+    def test_compute_statitics(self):
+        soc_list,temp_list = reciever.split_data_from_sender(self.consoleData)
         stats = reciever.compute_statitics(soc_list, temp_list, 3)
-        self.assertEqual(stats.max_soc, 50)
-        self.assertEqual(stats.min_soc, 10)
-        self.assertEqual(stats.max_temp, 7)
-        self.assertEqual(stats.min_temp, 1)
-        self.assertEqual(stats.mov_avg_soc, 16.0)
-        self.assertEqual(stats.mov_avg_temp, 2.4)
+        self.assertEqual(stats.max_soc, 58)
+        self.assertEqual(stats.min_soc, 29)
+        self.assertEqual(stats.max_temp, 309)
+        self.assertEqual(stats.min_temp, 300)
+        self.assertEqual(stats.mov_avg_soc, 23.2)
+        self.assertEqual(stats.mov_avg_temp, 183.6)
     
-    def test_process_data_from_sender(self):
-        stats = reciever.process_data_from_sender(self.data)
-        self.assertEqual(stats.max_soc, 100)
-        self.assertEqual(stats.min_soc, 10)
-        self.assertEqual(stats.max_temp, 30)
-        self.assertEqual(stats.min_temp, 1)
+    '''def test_process_data_from_sender(self):
+        stats = reciever.process_data_from_sender(self.consoleData)
+        self.assertEqual(stats.max_soc, 59)
+        self.assertEqual(stats.min_soc, 27)
+        self.assertEqual(stats.max_temp, 376)
+        self.assertEqual(stats.min_temp, 53)
         self.assertEqual(stats.mov_avg_soc, 55.2)
-        self.assertEqual(stats.mov_avg_temp, 8.8)
-        '''
+        self.assertEqual(stats.mov_avg_temp, 8.8)'''
     
     
 
