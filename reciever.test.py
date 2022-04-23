90738a57f7c37641a9a37a5f260bb6039434453a
Sudhir kumar <58461255+kumarSudhirTCQ@users.noreply.github.com>
2022-04-24 01:20:11 +0530

    Update reciever.test.py

--- reciever.test.py
+++ reciever.test.py
@@ -6,9 +6,20 @@ class reciever_test(unittest.TestCase):
     data = "SOC,10 20 50 100 80 90 60 44 15 54 65 54 10 20 50 100 80 90 60 44 15 54 65 54 10 20 50 100 80 90 60 44 15 54 65 54 10 20 50 100 80 90 60 44 15 54 65 54 88 66\nTEMPERATURE,1 4 7 9 2 5 2 20 15 30 1 4 7 9 2 5 2 20 15 30 1 4 7 9 2 5 2 20 15 30 1 4 7 9 2 5 2 20 15 30 1 4 7 9 2 5 2 20 15 30"
 
     def test_split_data_from_sender(self):
-        soc_list,temp_list = reciever.split_data_from_sender(self.data)
-        self.assertEqual(soc_list, [10 , 20 , 50 , 100 , 80 , 90 , 60 , 44 , 15 , 54 , 65 , 54 , 10 , 20 , 50 , 100 , 80 , 90 , 60 , 44 , 15 , 54 , 65 , 54 , 10 , 20 , 50 , 100 , 80 , 90 , 60 , 44 , 15 , 54 , 65 , 54 , 10 , 20 , 50 , 100 , 80 , 90 , 60 , 44 , 15 , 54 , 65 , 54 , 88 , 66])
-        self.assertEqual(temp_list, [1 , 4 , 7 , 9 , 2 , 5 , 2 , 20 , 15 , 30 , 1 , 4 , 7 , 9 , 2 , 5 , 2 , 20 , 15 , 30 , 1 , 4 , 7 , 9 , 2 , 5 , 2 , 20 , 15 , 30 , 1 , 4 , 7 , 9 , 2 , 5 , 2 , 20 , 15 , 30 , 1 , 4 , 7 , 9 , 2 , 5 , 2 , 20 , 15 , 30])
+        consoleData = "58.400000,300
+29.000000,309.000000
+29.000000,309.000000
+43.000000,137.000000
+34.000000,376.000000
+43.000000,376.000000
+33.000000,325.000000
+27.000000,53.000000
+48.000000,350.000000
+59.000000,350.000000"
+
+        soc_list,temp_list = reciever.split_data_from_sender(consoleData)
+        self.assertEqual(soc_list, [58, 29, 29, 43, 34, 43, 33, 27, 48, 59])
+        self.assertEqual(temp_list, [300, 309, 309, 137, 376, 376, 325, 53, 350, 350])
     
     def test_compute_statitics(self):
         soc_list,temp_list = reciever.split_data_from_sender(self.data)
@@ -32,7 +43,5 @@ class reciever_test(unittest.TestCase):
     
 
 
-
-
 if __name__ == '__main__':
-  unittest.main()
+  #unittest.main()
